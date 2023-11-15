37f9c978f4a300bbb6a995d82400db96249400ee
sstroemer <stefan.stroemer@ait.ac.at>
2023-11-15 16:19:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# Energy Modeling Technology Data
+
+This tool provides as slim Python wrapper to access the
+["technology-data"](https://github.com/PyPSA/technology-data) data set /
+[Snakemake](https://snakemake.readthedocs.io/en/stable/) workflow, maintained by the [PyPSA](https://github.com/PyPSA)
+team.
+
+> 🚨 🚨 Please check the license of the [`technology-data` repo](https://github.com/PyPSA/technology-data), especially
+regarding the applicable licences of various input (and therefore output) data files. Additional info can maybe found
+starting at [74](https://github.com/PyPSA/technology-data/issues/74) and
+[87](https://github.com/PyPSA/technology-data/pull/87). Be aware that this wrapper can in no way guarantee that the data
+being pulled from the repository, or the way that is being processed, or the code retrieved from the repository, or any
+related information is in accordance with applicable licenses.
+
+## Getting Started
+
+The following example assumes that you are using `conda` to create your environment, and `poetry` to manage your
+dependencies. Other ways should work in a similar fashion.
+
+Create an environment (skip if adding to an existing one; choose whatever Python version you want to use), installing
+`poetry` (skip if using a global installation or if you are already using `poetry`), initializing your `pyproject.toml`
+(consider also using `poetry new` instead of `poetry init` when starting a project), and then add `emtd` to your
+dependencies.
+
+```shell
+(base) $ conda create -n yourenvname python=3.11 -y
+(base) $ conda activate yourenvname
+
+(yourenvname) $ pip install poetry
+(yourenvname) $ poetry init
+(yourenvname) $ poetry add emtd
+```
+
+Now you can run the following examplatory code:
+
+```python
+# Use `./tmpdir` to store intermediate results.
+emtd = EMTD(target_dir="tmpdir")
+
+# Get all available parameters for the technology "solar" in 2030.
+emtd.parameters(2030, "solar")
+
+# Get the "lifetime" of "solar" in 2030.
+res = emtd.get(2030, "solar", "lifetime")
+
+# Try out:
+res["value"]
+res["unit"]
+res["source"]
+```
+
+## Reproducability
+To make sure everyone using your code will get the same results from `emtd`, it is advised to fix the data set to a 
+specific version. Consult the [release page](https://github.com/PyPSA/technology-data/releases) for available versions,
+then make sure to initialize using (e.g.):
+
+```python
+emtd = EMTD(target_dir="tmpdir", version="v0.6.2")
+```
+
+Make sure to include the `v` in the version string. Passing `"latest"` will put you on the current latest version of the
+technology data repository. Be aware that this can change anytime, and the next time you initialize `emtd`, it will try
+to update.
+
+## Configuring the Snakemake workflow
+To change parameters in the [Snakemake](https://snakemake.readthedocs.io/en/stable/) workflow, pass a `dict` to `EMTD`:
+
+```python
+emtd = EMTD(target_dir="tmpdir", params={"rate_inflation": 0.03})
+```
+
+This overwrites the defaults set by ["technology-data"](https://github.com/PyPSA/technology-data), or adds to it if the
+respective setting is not specified there. Consult
+[technology-data/config.yaml](https://github.com/PyPSA/technology-data/blob/master/config.yaml) for the current settings
+or hints at what can be changed. Also, consult their [documentation](https://technology-data.readthedocs.io/en/latest/).
+
+## Common Errors
+
+```
+subprocess.CalledProcessError: Command '['git', '-C', PosixPath('tmpdir'), 'pull']' returned non-zero exit status 1.
+```
+
+This error indicates an error during executing `git pull`. If you've previously used a `target_dir = "tmpdir` and
+pulled, e.g., `version="v0.6.1"`, and are now using `EMTD(target_dir="tmpdir")` (without version), the pull will fail;
+make sure to stick to one version, or use a different `target_dir` for managing different versions.
---
+++ emtd/emtd.py
@@ -0,0 +1,180 @@
+import os
+import re
+import logging
+from typing import Optional
+
+from pathlib import Path
+import subprocess
+import tempfile
+
+import yaml
+import pandas as pd
+
+
+class EMTD:
+    """
+    The EMTD (Energy Modeling Technology Data) class is designed for managing and processing technology data
+    for energy modeling. It provides functionalities for cloning the "technology data" repository of the PyPSA team,
+    https://github.com/PyPSA/technology-data, running snakemake workflows, and retrieving specific technology parameters
+    for given years.
+
+    Methods:
+        EMTD(target_dir: Optional[str], params: Optional[dict], version: str): Initializes the EMTD object.
+
+        technologies(self, year: int):
+                Retrieves the unique technologies available for a given year.
+        parameters(self, year: int, tech: str):
+                Retrieves the unique parameters for a given technology in a specific year.
+        get(self, year: int, tech: str, param: str):
+                Retrieves detailed data for a specific technology parameter in a given year.
+
+    Example Usage:
+        ```python
+        # Use `./tmpdir` to store intermediate results.
+        emtd = EMTD("tmpdir")
+
+        # Get all available parameters for the technology "solar" in 2030.
+        emtd.parameters(2030, "solar")
+
+        # Get the "lifetime" of "solar" in 2030.
+        res = emtd.get(2030, "solar", "lifetime")
+
+        # Try out:
+        res["value"]
+        res["unit"]
+        res["source"]
+        ```
+    """
+
+    def __init__(self, target_dir: Optional[str] = None, params: Optional[dict] = None, version: str = "latest") -> None:
+        """
+        Initializes the EMTD object with an optional target directory and parameters.
+
+        Args:
+            target_dir (Optional[str]): The directory to store and manage the downloaded technology data.
+            params (Optional[dict]): Parameters to be used in the snakemake workflow, overwriting the defaults.
+            version (str):
+                The data set version to use (e.g. "v0.6.2"); "latest" (the default) uses the most up-to-date version,
+                but may result in different data each time you call it. Consider fixing to a specific version!
+        """
+        self._logger = logging.getLogger("emtd")
+        self._results = dict()
+
+        if target_dir is None:
+            self._logger.warning("Consider specifying 'target_dir' to properly re-use previous work during snakemake")
+        self._prepare(Path(target_dir or tempfile.TemporaryDirectory().name), params or dict(), version)
+
+    def technologies(self, year: int) -> list:
+        """
+        Retrieves a list of unique technologies available for a given year.
+
+        Args:
+            year (int): The year for which to retrieve technology data.
+
+        Returns:
+            List[str]: A list of unique technology names,  or an empty list on error.
+        """
+        if year not in self._results:
+            self._logger.error("No results for year %d", year)
+            return list()
+        return list(self._results[year].technology.unique())
+
+    def parameters(self, year: int, tech: str) -> list:
+        """
+        Retrieves a list of unique parameters for a given technology and year.
+
+        Args:
+            year (int): The year for which to retrieve parameter data.
+            tech (str): The technology for which to retrieve parameter data.
+
+        Returns:
+            List[str]: A list of unique parameter names, or an empty list on error.
+        """
+        if year not in self._results:
+            self._logger.error("No results for [year=%d, tech=%s]", year, tech)
+            return list()
+
+        ret = self._results[year]
+        ret = ret.loc[ret.technology == tech]
+        return list(ret.parameter.unique())
+
+    def get(self, year: int, tech: str, param: str) -> dict:
+        """
+        Retrieves detailed data for a specific technology parameter in a given year.
+
+        Args:
+            year (int): The year for which to retrieve the data.
+            tech (str): The technology for which to retrieve the data.
+            param (str): The specific parameter to retrieve.
+
+        Returns:
+            dict: A dictionary containing the "value", "unit", "source", and "further description" of the parameter.
+                  Returns an empty dict on error.
+        """
+        if (tech not in self.technologies(year)) or (param not in self.parameters(year, tech)):
+            self._logger.error("No results for [year=%d, tech=%s, param=%s]", year, tech, param)
+            return dict()
+
+        ret = self._results[year]
+        ret = ret.loc[
+            (ret.technology == tech) & (ret.parameter == param), ["value", "unit", "source", "further description"]
+        ]
+        if len(ret) == 0:
+            self._logger.error("Zero (0) results for [year=%d, tech=%s, param=%s]", year, tech, param)
+            return dict()
+        if len(ret) > 1:
+            self._logger.error("Ambiguous results for [year=%d, tech=%s, param=%s]", year, tech, param)
+            return dict()
+
+        return ret.iloc[0].to_dict()
+
+    def _prepare(self, target_dir: Path, params: dict, version: str) -> None:
+        self._logger.info("Using temporary directory '%s' to manage 'technology-data'", target_dir)
+
+        self._clone_repository("https://github.com/PyPSA/technology-data.git", target_dir, version)
+
+        with open(target_dir / "__config.yaml", "w") as f:
+            yaml.dump(params, f)
+
+        self._run_snakemake(target_dir)
+
+        self._logger.info("Cleaning up temporary config file")
+        os.remove(target_dir / "__config.yaml")
+        self._logger.warning("The temporary directoy is not being deleted and could take up significant space")
+
+        self._logger.info("Parsing resulting outputs of snakemake workflow")
+        self._results = {
+            y: pd.read_csv(target_dir / "outputs" / f"costs_{y}.csv") for y in self._list_results(target_dir, "costs")
+        }
+
+    def _clone_repository(self, repo_url: str, target_dir: str, version: str) -> None:
+        if not os.path.exists(target_dir):
+            self._logger.info("Cloning 'technology-data")
+            os.makedirs(target_dir)
+            if version == "latest":
+                subprocess.run(["git", "clone", repo_url, target_dir], check=True, capture_output=True, text=True)
+            else:
+                subprocess.run(["git", "clone", repo_url, "--branch", version, target_dir], check=True, capture_output=True, text=True)
+        else:
+            self._logger.info("Updating 'technology-data'")
+            if version == "latest":
+                subprocess.run(["git", "-C", target_dir, "pull"], check=True, capture_output=True, text=True)
+            else:
+                subprocess.run(["git", "-C", target_dir, "pull", repo_url, version], check=True, capture_output=True, text=True)
+
+    def _run_snakemake(self, target_dir: Path) -> None:
+        self._logger.info("Starting snakemake workflow")
+        current_working_directory = os.getcwd()
+        os.chdir(target_dir)
+        subprocess.run(["snakemake", "-j1", "--configfile", "__config.yaml"], capture_output=True, text=True)
+        os.chdir(current_working_directory)
+        self._logger.info("Snakemake workflow done")
+
+    def _list_results(self, target_dir: Path, prefix: str) -> list:
+        years = []
+        for filename in os.listdir(target_dir / "outputs"):
+            match = re.findall(rf"{prefix}_(\d{{4}}).csv", filename)
+            if match:
+                years.append(int(match[0]))
+        self._logger.info("Found a total of %d applicable output files", len(years))
+        return years
